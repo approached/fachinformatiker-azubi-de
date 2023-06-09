739e22a1ec9ffa6b53095d7ece86f360c90073b7
Alexej Kloos <alexejkloos@gmail.com>
2023-06-09 14:32:55 +0200

    - src blog folder

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           // },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/approached/site-fachinformatiker-azubi',
             label: 'GitHub',
             position: 'right',
           },
